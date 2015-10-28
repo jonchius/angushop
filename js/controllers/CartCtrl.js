21084530feaf13f0ef9a3d5bd7cb3629b224174c
Jon Chiu <jonchius@gmail.com>
2015-10-28 16:39:59 -0400

    making cart upgrades

--- js/controllers/CartCtrl.js
+++ js/controllers/CartCtrl.js
@@ -9,10 +9,6 @@ function CartCtrl($uibModal, $uibModalInstance, cartService, cart){
 	this.cartService = cartService;
 	this.cart = this.cartService.cart;
 	this.order = {};
-	
-	this.total = 0;
-	this.tax = 0;
-	this.final_total = 0;
 
 }
 
@@ -21,23 +17,22 @@ CartCtrl.prototype.close = function() {
 }
 
 CartCtrl.prototype.clear = function() {
-	this.order = {}; this.cart = [];
-	this.total = this.tax = this.final_total = 0;
+	this.cartService.clearCart();
 }
 
 CartCtrl.prototype.checkout = function() {
+
 	var self = this;
 
-	this.final_total = this.findTotal();
 	this.order = {
 		cart: self.cart,
-		total: self.total,
-		tax: self.tax,
-		final_total: self.final_total
+		total: self.cartService.total,
+		tax: self.cartService.tax,
+		final_total: self.cartService.final_total
 	}
 
 	this.modalProperties = {
-	    animation:true,
+	    animation: true,
 	    templateUrl: 'templates/checkout.html',
 	    controller: 'CheckoutCtrl as Ctrl',
 	    resolve: {
@@ -54,21 +49,3 @@ CartCtrl.prototype.checkout = function() {
 
 }
 
-CartCtrl.prototype.findTotal = function(){
-
-	if (this.cart.length > 0) {
-
-		this.total = 0;
-		
-		for (var i=0; i < this.cart.length; i++){
-			this.total += parseFloat(this.cart[i].price);
-		}
-		
-		this.tax = this.total * TAX_RATE;
-		this.final_total = this.total + parseFloat(this.tax);
-	
-	} 
-
-	return parseFloat(this.final_total);
-
-}
--- js/services/cartService.js
+++ js/services/cartService.js
@@ -3,12 +3,41 @@ app.service('cartService', CartService);
 function CartService() {
 
 	this.cart = [];
+	this.total = 0;
+	this.tax = 0;
+	this.final_total = 0;
 
 }
 
 CartService.prototype.addtoCart = function(product, quantity) {
-
 	this.cart.push({product:product, quantity:quantity});
-	console.log(this.cart);
+}
+
+CartService.prototype.clearCart = function() {
+	this.final_total = this.tax = this.total = 0; 
+	this.cart = []; 
+}
+
+CartService.prototype.checkout = function() {
+
+
+}
+
+CartService.prototype.findTotal = function(){
+
+	if (this.cart.length > 0) {
+
+		this.total = 0;
+		
+		for (var i=0; i < this.cart.length; i++){
+			this.total += parseFloat(this.cart[i].product.price * this.cart[i].quantity);
+		}
+		
+		this.tax = this.total * TAX_RATE;
+		this.final_total = parseFloat(this.total + this.tax);
 	
+	} 
+
+	return parseFloat(this.final_total);
+
 }
